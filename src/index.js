f77a454f8dbbfb69ac99f210d6b2045debd45755
ishtiyaque.jaffer@iifl.com <ishtiyaque.jaffer>
2023-12-30 12:05:36 +0530

    update

--- src/index.js
+++ src/index.js
@@ -2,59 +2,31 @@ const express = require("express");
 const app = express();
 const env = require("dotenv");
 const cors = require("cors");
-const http = require("http");
 const port = process.env.PORT || 2000;
 
 //ENVIRONMENT VARIABLE/CONSTANTS
 env.config();
 
-async function onServerStart() {
-  console.log(`Server listening on port ${port}`);
+//MONGODB CONNECTION
+require("./db/conn");
 
-  // Test DB
-  require("./db/conn");
-}
+const userRoutes = require("./routes/user");
+const taskRoutes = require("./routes/task");
+const attendanceRoutes = require("./routes/attendance");
 
-function main() {
-  const server = http.createServer(app);
-  server.listen(port, onServerStart);
-  const socketio = require("socket.io");
-  const io = socketio(server, {
-    cors: {
-      origin: "*",
-      methods: ["GET", "POST"],
-    },
-  });
-  const socketEvents = require("./socket");
-  socketEvents(io);
+app.use(cors());
+app.use(express.json());
 
-  const userRoutes = require("./routes/user");
-  const workspaceRoutes = require("./routes/workspace");
-  const postRoutes = require("./routes/post");
-  const classroomRoutes = require("./routes/classroom");
-  const taskRoutes = require("./routes/task");
-  const attendanceRoutes = require("./routes/attendance");
+app.get("/", (req, res) => {
+  res.send("Task Manager Server is up and running");
+});
 
-  app.use(cors());
-  app.use(express.json());
+app.use("/api", userRoutes);
+app.use("/api", taskRoutes);
+app.use("/api", attendanceRoutes);
 
-  app.get("/", (req, res) => {
-    res.send("ThoughtMuseum Server is up and running");
-  });
-
-  app.use("/api", userRoutes);
-  app.use("/api", workspaceRoutes);
-  app.use("/api", postRoutes);
-  app.use("/api", classroomRoutes);
-  app.use("/api", taskRoutes);
-  app.use("/api", attendanceRoutes);
-
-
-}
-
-main();
-
-
-
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 
